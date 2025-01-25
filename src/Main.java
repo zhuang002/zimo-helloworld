985f85f7d577fb44b69e7a45fc53444b8464dab3
zhuang002 <zhuang001@gmail.com>
2025-01-24 20:55:13 -0500

    Only output

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		
+		int a=70;
+		int b=30;
+		int c = a+b;
+		System.out.print("120+30=");
+		System.out.println(c);
+		
+		System.out.println(a+"+"+b+"="+c);
+		
+		int d;
+		int e;
+		int f;
+		
+		d=20;
+		e=18;
+		f = d+e;
+		
+		String line;
+		line = d+"+"+e+"="+f;
+		System.out.println(line);
+
+	}
+
+}
