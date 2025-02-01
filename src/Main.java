b72a780c512eff3daa6dd318d2e44b15a9dac203
zhuang002 <zhuang001@gmail.com>
2025-01-31 20:56:18 -0500

    boolean variables and operations

--- src/Main.java
+++ src/Main.java
@@ -3,57 +3,38 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		boolean a,b,c,d;  // names in purple are reserved words(保留字). They have special meaning in 
+		// programming language.
 		
 		Scanner sc = new Scanner(System.in);
+		System.out.print("Please input boolean value a:");
+		a = sc.nextBoolean();
+		System.out.print("Please input boolean value b:");
+		b = sc.nextBoolean();
+		System.out.print("Please input boolean value c:");
+		c = sc.nextBoolean();
 		
-		System.out.print("Please input a:");
-		double a=sc.nextDouble();
-		System.out.print("Please input b:");
-		double b=sc.nextDouble();
+		d = a && b; // && is the operator for And operation. 
+		System.out.println("a and b is:" + d);
 		
-		double c = a+b;
-		System.out.println("a is "+a+". b is "+b+". a+b="+c);
+		d = a || b; // || is the operator for Or operation.
+		System.out.println("a or b is:" + d);
 		
-		c = a-b;
-		System.out.println("a is "+a+". b is "+b+". a-b="+c);
+		d = a && b || c;
+		System.out.println("(a && b || c) is:" + d);
 		
-		c = a*b;
-		System.out.println("a is "+a+". b is "+b+". a*b="+c);
-
-		c = a/b;
-		System.out.println("a is "+a+". b is "+b+". a/b="+c);
+		d = a || (b && c);
+		System.out.println("(a || (b && c)) is:" + d);
 		
-		System.out.print("c is "+c+".");
-		c = c*b;
-		System.out.println(" b is "+b+". c*b="+c);
+		d = (a || b) && (a || c);
+		System.out.println("((a || b) && (a || c)) is:" + d);
 		
-		// a, b, c are variables. the name of a variable must start with an alphabet. a1 b2 are valid
-		// variable names. studentName is also a valid variable name. 1b, 1student are not valid 
-		// variable names. &bc, *bb are not valid variable names either. 
+		d = !(a || c);
+		System.out.println("(!(a || c)) is:" + d);
 		
-		// here = is assignment(赋值) statement(语句). it is not to compare 2 numbers. 
+		d = !a && !c;
+		System.out.println("(!a && !c) is:" + d);
 		
-		// next problem: can an integer be converted to a double? answer: yes.
-		
-		int i = 4;
-		a = i;
-		
-		System.out.println("i is "+i+". a is "+a);
-		
-		// next problem: can a double be converted to an integer? answer: yes.
-		
-		a = 4.3;
-		// i = a; this is wrong. because you may miss something when convert double to int.
-		// if we force to convert, then we need type cast. (强制转换类型)
-		i = (int)a;
-		System.out.println("a is "+a+". i is "+i);
-		
-		// using // before a line means the line is comment.
-		//next problem: how to cast a negative double.
-		a = -4.3;
-		i = (int)a;
-		System.out.println("a is "+a+". i is "+i);
 		
 	}
 
