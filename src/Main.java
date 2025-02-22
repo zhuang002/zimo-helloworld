a663139b8001c276c387934337f80a386ef11639
zhuang002 <zhuang001@gmail.com>
2025-02-21 21:00:07 -0500

    char and String type, + operation, ASCII code

--- src/Main.java
+++ src/Main.java
@@ -4,26 +4,68 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		
-		int a=70;
-		int b=30;
-		int c = a+b;
-		System.out.print("120+30=");
+		// concept of char
+		// int, char, boolean, double are primitive types.
+		char a = 'A';
+		char b = '#';
+		
+		System.out.println(a);
+		System.out.println(b);
+		
+		// concept of string
+		//String, Scanner are a Classes
+		String s1 = "abcde";
+		String s2 = "BjSdf ddassdf";
+		
+		System.out.println(s1);
+		System.out.println(s2);
+		
+		
+		// the + operation of Strings;
+		String s = s1+s2;
+		System.out.println(s);
+		
+		
+		// the + operation of String and char.
+		s = s1+a;
+		System.out.println(s);
+		
+		s = a+s1;
+		System.out.println(s);
+		
+		// the + operation of String and int
+		s = s1+45;
+		System.out.println(s);
+		
+		s= 45+s1;
+		System.out.println(s);
+		
+		// the + operation of String and boolean
+		s = s1+true;
+		System.out.println(s);
+		
+		s= false + s1;
+		System.out.println(s);
+		
+		// the + operation of String and double;
+		s = s1+2.5/3;
+		System.out.println(s);
+		
+		
+		// ASCII code for characters.
+		int i1 = a+1;
+		System.out.println(i1);
+		
+		System.out.println(a+1);
+		
+		double d1 = a+1.5;
+		System.out.println(d1);
+		
+		char c = (char) (a+1);
 		System.out.println(c);
 		
-		System.out.println(a+"+"+b+"="+c);
 		
-		int d;
-		int e;
-		int f;
-		
-		d=20;
-		e=18;
-		f = d+e;
-		
-		String line;
-		line = d+"+"+e+"="+f;
-		System.out.println(line);
-
+		System.out.println(b+1);
 	}
 
 }
